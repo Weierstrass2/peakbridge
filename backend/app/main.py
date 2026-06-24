dc23ea0abcaee63570e0559e6847c44c4361ed3a
Weierstrass2 <young3014119@gmail.com>
2026-06-25 03:28:04 +0900

    CORS 임시 전체 허용

--- backend/app/main.py
+++ backend/app/main.py
@@ -65,7 +65,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.ALLOWED_ORIGINS,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
