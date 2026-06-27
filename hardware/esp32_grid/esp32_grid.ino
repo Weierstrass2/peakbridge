4522a442033becc9507878862fc4aa6bf6806d53
Weierstrass2 <parktaejun4312@gmail.com>
2026-06-28 01:11:14 +0900

    ESP32 #1 코드 보완 - 디버깅/LED/피크감지

--- hardware/esp32_grid/esp32_grid.ino
+++ hardware/esp32_grid/esp32_grid.ino
@@ -14,21 +14,35 @@ const char* MQTT_TOPIC = "peakbridge/building-A/grid/current";
 
 const int CT_PIN = 34;
 const float CT_CALIBRATION = 30;
+const int LED_PIN = 2;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 EnergyMonitor emon;
 
+unsigned long previousMillis = 0;
+long ledInterval = 1000;
+bool ledState = LOW;
+
 void setupWiFi() {
+  int attempts = 0;
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED &amp;&amp; attempts &lt; 10) {
+    Serial.println("Wi-Fi 연결 중...");
     delay(500);
+    attempts++;
+  }
+  
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Wi-Fi 연결 실패, ESP32 재시작...");
+    ESP.restart();
   }
 }
 
 void reconnect() {
   while (!client.connected()) {
     if (client.connect(DEVICE_ID)) {
+      Serial.println("MQTT 연결 완료");
     } else {
       delay(5000);
     }
@@ -36,6 +50,9 @@ void reconnect() {
 }
 
 void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  
   emon.current(CT_PIN, CT_CALIBRATION);
   setupWiFi();
   client.setServer(MQTT_SERVER, MQTT_PORT);
@@ -48,12 +65,30 @@ void loop() {
   client.loop();
 
   double Irms = emon.calcIrms(1480);
+  
+  Serial.println("전류: " + String(Irms) + "A");
+  
+  if (Irms &gt; 15.0) {
+    Serial.println("⚠️ 피크 감지! " + String(Irms) + "A");
+    ledInterval = 200;
+  } else {
+    ledInterval = 1000;
+  }
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis &gt;= ledInterval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(LED_PIN, ledState);
+  }
 
   StaticJsonDocument&lt;256&gt; doc;
   doc["value"] = Irms;
   doc["unit"] = "A";
   doc["device_id"] = DEVICE_ID;
   doc["building_id"] = BUILDING_ID;
+  doc["sensor_type"] = "grid_current";
+  doc["timestamp"] = millis();
 
   String payload;
   serializeJson(doc, payload);
